a7cc360fe6b9d755b18bb1774189603733fbdb21
Aldi-H <aldihimawan88@gmail.com>
2023-04-01 19:38:04 +0700

    chore: edit route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import FeeType from "./pages/FeeType";
+import FeeTypeDetail from "./pages/FeeTypeDetail";
+import FeeTypeEdit from "./pages/FeeTypeEdit";
+import FeeTypeCreate from "./pages/FeeTypeCreate";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<FeeType />} />
+        <Route path="/fee-type-create" element={<FeeTypeCreate />} />
+        <Route path="/fee-type-detail/:id" element={<FeeTypeDetail />} />
+        <Route path="/fee-type-edit/:id" element={<FeeTypeEdit />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </Router>
   );
 }
 
